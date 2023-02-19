72e0281b30522c577ef63735047ab1538c19275b
Raul Naupari <raulnq@gmail.com>
2023-02-19 09:49:48 -0500

    initial code

---
+++ calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿namespace calculator;
+public class Calculator
+{
+    public decimal Multiply(decimal a, decimal b)
+    {
+        return a * b;
+    }
+
+    public decimal Divide(decimal a, decimal b)
+    {
+        return a / b;
+    }
+
+    public decimal Add(decimal a, decimal b)
+    {
+        return a + b;
+    }
+
+    public decimal Subtrac(decimal a, decimal b)
+    {
+        return a - b;
+    }
+}
---
+++ tests/Tests.cs
@@ -0,0 +1,60 @@
+using calculator;
+
+namespace tests;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    [DataRow(1, 1, 1)]
+    [DataRow(2, 3, 6)]
+    [DataRow(7, 5, 35)]
+    public void x_multiplied_by_y_should_be_z(int x, int y, int z)
+    {
+        var sut = new Calculator();
+
+        var r = sut.Multiply(x, y);
+
+        Assert.AreEqual(z, r);
+    }
+
+    [TestMethod]
+    [DataRow(1, 1, 1)]
+    [DataRow(10, 5, 2)]
+    [DataRow(24, 3, 8)]
+    public void x_divided_by_y_should_be_z(int x, int y, int z)
+    {
+        var sut = new Calculator();
+
+        var r = sut.Divide(x, y);
+
+        Assert.AreEqual(z, r);
+    }
+
+
+    [TestMethod]
+    [DataRow(0, 0, 0)]
+    [DataRow(5, 3, 8)]
+    [DataRow(2, 9, 11)]
+    public void x_plus_y_should_be_z(int x, int y, int z)
+    {
+        var sut = new Calculator();
+
+        var r = sut.Add(x, y);
+
+        Assert.AreEqual(z, r);
+    }
+
+    [TestMethod]
+    [DataRow(0, 0, 0)]
+    [DataRow(5, 1, 4)]
+    [DataRow(11, 5, 6)]
+    public void zero_minus_zero_should_be_zero(int x, int y, int z)
+    {
+        var sut = new Calculator();
+
+        var r = sut.Subtrac(x, y);
+
+        Assert.AreEqual(z, r);
+    }
+}
